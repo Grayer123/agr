e3390122ed41a93675756b08b04dc8d071ebd235
Hongmin Wang <ranwang568@gmail.com>
2016-07-06 22:16:44 -0400

    [371. Sum of Two Integers][Accepted]committed by LeetCode Extension

---
+++ 371-Sum-of-Two-Integers/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int GetSum(int a, int b) {
+        //bit manipulation
+        //TC:O(1); SC:O(1)
+        int val1 = a & b;
+        int val2 = a ^ b;
+        val1 = (val1 << 1);
+        //return (val1 << 1) | val2;
+        if((val1 & val2) == 0){
+            return val1 | val2;
+        }
+        else{
+            return GetSum(val1, val2);
+        }
+    }
+}
