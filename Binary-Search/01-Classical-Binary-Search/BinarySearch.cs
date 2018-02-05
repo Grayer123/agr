afb72d0f6e328ba6817f6011eb5a5efa2966ebc1
Hongmin Wang <hongmin.wang@reisystems.com>
2018-02-05 01:21:39 -0500

    Checked in the generic implementation for classical binary search.

---
+++ Binary-Search/01-Classical-Binary-Search/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassicBinarySearch
+{
+    public static class BinarySearch
+    {
+        public static int DoBinarySearch<T>(T[] nums, T target) where T: IComparable<T>
+        {
+            if(nums.Length == 0)
+            {
+                return -1; // corner case
+            }
+            int start = 0, end = nums.Length - 1;
+            int mid;
+
+            while(start + 1 < end)
+            {
+                mid = start + (end - start) / 2; // prevent overflow like (start+end)/2
+
+                if(nums[mid].CompareTo(target) == 0) // nums[mid] == target
+                {
+                    end = mid; // it might have multiple matches, need to find the first one
+                }
+                else if(nums[mid].CompareTo(target) < 0) // nums[mid] < target
+                {
+                    start = mid;
+                }
+                else // nums[mid] > target
+                {
+                    end = mid;
+                }
+            }
+            if(nums[start].CompareTo(target) == 0) // nums[start] == target
+            {
+                return start;
+            }
+            if (nums[end].CompareTo(target) == 0) // nums[end] == target
+            {
+                return end;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Binary-Search/01-Classical-Binary-Search/Question.md
@@ -0,0 +1,17 @@
+# 01. Classical Binary Search
+
+For a given sorted array (ascending order) and a target number, find the first index of this number in **O(log n)** time complexity.
+If the target number does not exist in the array, return **-1**.
+ 
+**Example:** 
+```
+If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
+```
+
+**Challenge:** 
+```
+If the count of numbers is bigger than 2^32, can your code work properly?
+```
+
+---
+
