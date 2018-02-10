9a0c18a35dfb30b26d6b9a089ec9137d91af338f
Hongmin Wang <hongmin.wang@reisystems.com>
2018-02-10 16:55:31 -0500

    Checked in two solutions.

---
+++ Binary-Search/Medium/658-Find K Closest Elements/Solution_CombinedBinarySearch.cs
@@ -0,0 +1,25 @@
+//combined binary search with two pointers at the same time
+public class Solution {
+    public IList<int> FindClosestElements(int[] arr, int k, int x) {
+        //tc:O(nlogn); sc:O(k)
+        int len = arr.Length;
+        int start = 0;
+        int end = len - k; //ensure k elems between arr[len-k] - arr[len-1]
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (Math.Abs(x - arr[mid]) <= Math.Abs(arr[mid + k] - x)) //window size: k; start may advance 1, so keep k+1 elems at first
+            { 
+                end = mid;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        int[] res = new int[k];
+
+        Array.Copy(arr, start, res, 0, k);
+        return new List<int>(res);
+    }
+}
