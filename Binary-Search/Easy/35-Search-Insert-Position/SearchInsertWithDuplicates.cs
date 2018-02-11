fcbf9af88e5a5be8add727b2a9aed8802b72ff07
Hongmin Wang <hongmin.wang@reisystems.com>
2018-02-10 20:31:22 -0500

    Simplified the solution.

--- Binary-Search/Easy/35-Search-Insert-Position/SearchInsertWithDuplicates.cs
+++ Binary-Search/Easy/35-Search-Insert-Position/SearchInsertWithDuplicates.cs
@@ -1,31 +1,25 @@
 public class Solution {
-    public int SearchInsertWithDuplicates(int[] nums, int target)
-        {
-            //tc:O(lgn); sc:O(1)
-            if (nums.Length == 0)
-            {
-                return 0; //corner case
+    public int SearchInsert(int[] nums, int target) {
+        //tc:O(lgn); sc:O(1)
+        //find the pos of firstNum >= target; then insert target to this pos
+        if(nums.Length == 0){
+            return 0; //corner case
+        }
+        int start = 0, end = nums.Length - 1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] < target){
+                start = mid + 1;
             }
-            int start = 0, end = nums.Length - 1;
-            while (start < end)
-            {
-                int mid = start + (end - start) / 2;
-                if (nums[mid] < target)
-                {
-                    start = mid + 1;
-                }
-                else
-                {
-                    end = mid;
-                }
-            }
-            if (nums[start] >= target)
-            {
-                return start;
-            }
-            else
-            {
-                return start + 1;
+            else{
+                end = mid;
             }
         }
+        if(nums[start] >= target){
+            return start;
+        }
+        else {
+            return nums.Length;
+        }
+    }
 }
