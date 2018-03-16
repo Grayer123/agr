2d1708036511e4d95b0571c2131f92f7d57966a2
Hongmin Wang <hongmin.wang@reisystems.com>
2018-03-16 11:55:20 -0400

    Corrected space complexity.

--- Array/Easy/796-Rotate-String/solution_neat.cs
+++ Array/Easy/796-Rotate-String/solution_neat.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public bool RotateString(string A, string B) {
-        //TC:O(n); SC:O(1)
+        //TC:O(n); SC:O(n)
         if(A == null || B == null || A.Length != B.Length){ //corner case
             return false;
         }
-        return (A + A).Contains(B);
-        
+        return (A + A).Contains(B);       
     }
 }
