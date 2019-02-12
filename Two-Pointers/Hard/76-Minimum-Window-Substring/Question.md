87d2a6f320baf3dd62c40773b7ebc4f4755a8d4d
Hongmin Wang <hongmin.wang@reisystems.com>
2019-02-11 23:34:13 -0500

    Check in a Hard level question with solution.

---
+++ Two-Pointers/Hard/76-Minimum-Window-Substring/Question.md
@@ -0,0 +1,34 @@
+# 76. Minimum Window Substring
+
+[Original Page](https://leetcode.com/problems/minimum-window-substring/)
+
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+For example,  
+**S** = `"ADOBECODEBANC"`  
+**T** = `"ABC"`  
+
+Minimum window is `"BANC"`.
+
+**Note:**  
+If there is no such window in S that covers all characters in T, return the empty string `""`.
+
+If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+
+<div>
+
+<div id="company_tags" class="btn btn-xs btn-warning">Hide Company Tags</div>
+
+<span class="hidebutton" style="display: inline;">[LinkedIn](/company/linkedin/) [Facebook](/company/facebook/)</span></div>
+
+<div>
+
+<div id="tags" class="btn btn-xs btn-warning">Hide Tags</div>
+
+<span class="hidebutton" style="display: inline;">[Hash Table](/tag/hash-table/) [Two Pointers](/tag/two-pointers/) [String](/tag/string/)</span></div>
+
+<div>
+
+<div id="similar" class="btn btn-xs btn-warning">Hide Similar Problems</div>
+
+<span class="hidebutton" style="display: inline;">[(H) Substring with Concatenation of All Words](/problems/substring-with-concatenation-of-all-words/) [(M) Minimum Size Subarray Sum](/problems/minimum-size-subarray-sum/) [(H) Sliding Window Maximum](/problems/sliding-window-maximum/)</span></div>
---
+++ Two-Pointers/Hard/76-Minimum-Window-Substring/solution.cs
@@ -0,0 +1,52 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        // two pointers(sliding window); hash table
+        //TC:O(n); SC:O(n)
+        if(s.Length < t.Length) {
+            return String.Empty;
+        }
+        Dictionary<char, int> dictT = new Dictionary<char, int>();
+        foreach(char c in t) {
+            dictT[c] = dictT.ContainsKey(c) ? ++dictT[c] : 1;
+        }
+        int left = 0, right = 0;
+        int numOfMatched = 0;
+        int[] res = {-1, -1};
+        Dictionary<char, int> windowCounts = new Dictionary<char, int>();
+        while(right < s.Length) {
+            while(numOfMatched < dictT.Count && right < s.Length) {
+                if(dictT.ContainsKey(s[right])) {
+                    windowCounts[s[right]] = windowCounts.ContainsKey(s[right]) ? ++windowCounts[s[right]] : 1;
+                    if(dictT[s[right]] == windowCounts[s[right]]) {
+                        numOfMatched++;
+                    }
+                    if(numOfMatched == dictT.Count) {
+                        break;
+                    }
+                }
+                right++;
+            }
+            if(right >= s.Length) {
+                break;
+            }
+            while(numOfMatched == dictT.Count && left <= right - t.Length + 1) {
+                if(dictT.ContainsKey(s[left])) {
+                    windowCounts[s[left]]--;
+                    if(windowCounts[s[left]] < dictT[s[left]]) {
+                        numOfMatched--;
+                    }
+                }
+                left++;
+            }
+            if(res[0] == - 1 || res[1] - res[0] > right - left) {
+                res[0] = left - 1;
+                res[1] = right;
+            }
+            right++;
+        }
+        if(res[0] == -1) {
+            return String.Empty;
+        }
+        return s.Substring(res[0], res[1] - res[0] + 1);
+    }
+}
