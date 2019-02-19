2952946f00fe5e635397be54f22b93639345ed1c
Hongmin Wang <hongmin.wang@reisystems.com>
2019-02-19 11:43:32 -0500

    Modified followup solution for q234.

--- Linked-List/Easy/234-Palindrome-Linked-List/solution_followup.cs
+++ Linked-List/Easy/234-Palindrome-Linked-List/solution_followup.cs
@@ -1,56 +1,50 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
  */
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        //TC:O(n); SC:O(1)
-        if(!head){
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        // use list as auxiliary 
+        // TC:O(n); SC:O(n)
+        if(head == null || head.next == null) {
             return true;
         }
-        ListNode* mid = findMiddle(head);
-        ListNode* tmp = mid->next;
-        mid->next = nullptr; //disconnect
-        ListNode* newHead = reverseList(tmp);
-        while(newHead){
-            if(newHead->val != head->val){
+        ListNode mid = FindMiddle(head); 
+        ListNode cur = mid.next;
+        mid.next = null; // disconnect
+        ListNode newHead = RotateList(cur);
+        while(head != null && newHead != null) {
+            if(head.val != newHead.val) {
                 return false;
-            }
-            newHead = newHead->next;
-            head = head->next;
+            } 
+            head = head.next;
+            newHead = newHead.next;
         }
         return true;
-    }
-private:
-    ListNode* findMiddle(ListNode* head){
-        if(!head){
-            return nullptr;
-        }
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
+        
     }
     
-    ListNode* reverseList(ListNode* head){
-        if(!head){
-            return nullptr;
+    private ListNode FindMiddle(ListNode head) {
+        ListNode left = head, right = head;
+        while(right.next != null && right.next.next != null) {
+            left = left.next;
+            right = right.next.next;
         }
-        ListNode* prev = nullptr;
-        while(head){
-            ListNode* tmp = head->next;
-            head->next = prev;
+        return left;
+    }
+    
+    private ListNode RotateList(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
             prev = head;
             head = tmp;
         }
         return prev;
     }
-};
+}
