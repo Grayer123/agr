992a61b0abc3c747264cd2da9fb79d42a51e14d6
Hongmin Wang <hongmin.wang@reisystems.com>
2019-02-20 12:23:30 -0500

    Check in dfs solution for question 133 clone graph.

---
+++ BFS/Medium/133-Clone-Graph/solution_dfs.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for undirected graph.
+ * public class UndirectedGraphNode {
+ *     public int label;
+ *     public IList<UndirectedGraphNode> neighbors;
+ *     public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode CloneGraph(UndirectedGraphNode node) {
+        //dfs + hashtable
+        // tc:O(n); sc:O(n)
+        if(node == null) {
+            return node;
+        }
+        Dictionary<UndirectedGraphNode, UndirectedGraphNode> dict = new Dictionary<UndirectedGraphNode, UndirectedGraphNode>();
+        return CloneNode(node, dict);
+    }
+    
+    private UndirectedGraphNode CloneNode(UndirectedGraphNode node, Dictionary<UndirectedGraphNode, UndirectedGraphNode> dict) {
+        dict[node] = new UndirectedGraphNode(node.label);
+        foreach(var nd in node.neighbors) {
+            if(!dict.ContainsKey(nd)) {
+                CloneNode(nd, dict);
+            }
+            dict[node].neighbors.Add(dict[nd]);
+        }
+        return dict[node];
+    }
+}
