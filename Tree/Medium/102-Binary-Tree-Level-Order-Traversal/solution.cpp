82d294433aaf1420a85900c91f84046de482f171
Hongmin Wang <hongmin.wang@reisystems.com>
2019-02-21 00:56:06 -0500

    Check in new question and bfs solution for question 102.

---
+++ Tree/Medium/102-Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        // bfs + level order traversal
+        // tc:O(n); sc:O(n)
+        if(root == null) {
+            return new List<IList<int>>();
+        }
+        IList<IList<int>> res = new List<IList<int>>();
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while(queue.Count > 0) {
+            int levelSize = queue.Count;
+            IList<int> list = new List<int>();
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.Dequeue();
+                list.Add(node.val);
+                if(node.left != null) {
+                    queue.Enqueue(node.left);
+                }
+                if(node.right != null) {
+                    queue.Enqueue(node.right);
+                }
+            }
+            res.Add(list);
+        }
+        return res;
+    }
+}
