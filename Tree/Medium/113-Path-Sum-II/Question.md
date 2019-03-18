7ef5c2abf379d18170155b9e11ce60e119b1d983
Hongmin Wang <hongmin.wang@reisystems.com>
2019-03-17 22:16:50 -0400

    Check in new question and its initial solution for q113.

---
+++ Tree/Medium/113-Path-Sum-II/Question.md
@@ -0,0 +1,43 @@
+# 113. Path Sum II
+
+[Original Page](https://leetcode.com/problems/path-sum-ii/)
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:  
+Given the below binary tree and `sum = 22`,
+
+<pre>              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+</pre>
+
+return  
+
+<pre>[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+</pre>
+
+<div>
+
+<div id="company_tags" class="btn btn-xs btn-warning">Hide Company Tags</div>
+
+<span class="hidebutton" style="display: inline;">[Bloomberg](/company/bloomberg/)</span></div>
+
+<div>
+
+<div id="tags" class="btn btn-xs btn-warning">Show Tags</div>
+
+<span class="hidebutton">[Tree](/tag/tree/) [Depth-first Search](/tag/depth-first-search/)</span></div>
+
+<div>
+
+<div id="similar" class="btn btn-xs btn-warning">Show Similar Problems</div>
+
+<span class="hidebutton">[(E) Path Sum](/problems/path-sum/) [(E) Binary Tree Paths](/problems/binary-tree-paths/)</span></div>
---
+++ Tree/Medium/113-Path-Sum-II/solution_initial.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> PathSum(TreeNode root, int sum) {
+        // divide and conquer
+        // TC:O(n); SC:O(h)
+        if(root == null) { // corner case
+            return new List<IList<int>>();
+        }
+        List<IList<int>> res = new List<IList<int>>();
+        List<int> path = new List<int>();
+        FindSumPath(root, sum, 0, res, path); 
+        return res;
+    }
+    
+    private void FindSumPath(TreeNode node, int target, int pathSum, List<IList<int>> res, List<int> path) {
+        if(node == null) {
+            return;
+        }
+        pathSum += node.val;
+        path.Add(node.val);
+        if(node.left == null && node.right == null && pathSum == target) { // leaf node
+            res.Add(new List<int>(path));
+            return;
+        }
+        FindSumPath(node.left, target, pathSum, res, path);
+        if(node.left != null) {
+            path.RemoveAt(path.Count - 1); // remove node from left node
+        }
+        
+        FindSumPath(node.right, target, pathSum, res, path);
+        if(node.right != null) {
+            path.RemoveAt(path.Count - 1); // remove node from right node
+        }
+    }
+}
