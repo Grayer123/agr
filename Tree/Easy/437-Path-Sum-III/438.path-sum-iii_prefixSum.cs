df90ae2918b6461d6cb4c8f16a980dfe047d41a3
Hongmin Wang <hongmin.wang@reisystems.com>
2019-03-18 22:18:52 -0400

    Check in prefix sum - hash table solution for q437.

---
+++ Tree/Easy/437-Path-Sum-III/438.path-sum-iii_prefixSum.cs
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=437 lang=csharp
+ *
+ * [437] Path Sum III
+ *
+ * https://leetcode.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Easy (42.06%)
+ * Total Accepted:    95.3K
+ * Total Submissions: 226.5K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * You are given a binary tree in which each node contains an integer value.
+ * 
+ * Find the number of paths that sum to a given value.
+ * 
+ * The path does not need to start or end at the root or a leaf, but it must go
+ * downwards
+ * (traveling only from parent nodes to child nodes).
+ * 
+ * The tree has no more than 1,000 nodes and the values are in the range
+ * -1,000,000 to 1,000,000.
+ * 
+ * Example:
+ * 
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ * 
+ * ⁠     10
+ * ⁠    /  \
+ * ⁠   5   -3
+ * ⁠  / \    \
+ * ⁠ 3   2   11
+ * ⁠/ \   \
+ * 3  -2   1
+ * 
+ * Return 3. The paths that sum to 8 are:
+ * 
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3. -3 -> 11
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int PathSum(TreeNode root, int sum) {
+        // divide and conquer + prefix sum
+        // TC:O(n); SC:O(n)
+        if(root == null) {
+            return 0;
+        }
+        int res = 0;
+        Dictionary<int, int> prefixSum = new Dictionary<int, int>(); // dictionary to keep all the prefix
+        prefixSum.Add(0, 1); // add a base: so that when firstNode.val == target => could find a prefix 0, times => 1 
+        FindPaths(root, prefixSum, sum, 0, ref res);
+        return res;
+    }
+    
+    private void FindPaths(TreeNode node, Dictionary<int, int> prefixSum, int target, int curSum, ref int res) {
+        if(node == null) {
+            return;
+        }
+        curSum += node.val;
+        if(prefixSum.ContainsKey(curSum - target)) { // to see if (curSum - target) appears in prefixSum
+            res += prefixSum[curSum - target];  // how many times
+        }
+        prefixSum[curSum] = prefixSum.ContainsKey(curSum) ? ++prefixSum[curSum] : 1;
+        FindPaths(node.left, prefixSum, target, curSum, ref res);
+        FindPaths(node.right, prefixSum, target, curSum, ref res);
+        prefixSum[curSum]--; // backtrack
+    }
+}
+
