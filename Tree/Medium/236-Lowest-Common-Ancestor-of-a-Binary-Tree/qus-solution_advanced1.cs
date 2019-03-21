65e9f359465f39475fa34db8881e28a58d3302eb
Hongmin Wang <hongmin.wang@reisystems.com>
2019-03-20 20:23:29 -0400

    Check in a ResultType solution which not requiring p, q in the tree for q236.

---
+++ Tree/Medium/236-Lowest-Common-Ancestor-of-a-Binary-Tree/qus-solution_advanced1.cs
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=236 lang=csharp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (35.71%)
+ * Total Accepted:    253.5K
+ * Total Submissions: 707.8K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given
+ * nodes in the tree.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the binary tree.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class ResultType {
+    public bool pExisted;
+    public bool qExisted;
+    public TreeNode lca;
+    
+    public ResultType(bool p, bool q, TreeNode l) {
+        this.pExisted = p;
+        this.qExisted = q;
+        this.lca = l;
+    }
+}
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // divide and conquer
+        // tc:O(n); sc:O(h)
+        // p and q could be or not be in the tree
+        if(root == null) { // corner case
+            return root;
+        }
+        return FindLCA(root, p, q).lca;
+    }
+    
+    private ResultType FindLCA(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return new ResultType(false, false, null);
+        }
+        ResultType left = FindLCA(root.left, p, q);
+        if(left.lca != null) {
+            return left;
+        }
+        
+        ResultType right = FindLCA(root.right, p, q);        
+        if(right.lca != null) {
+            return right;
+        }
+        bool pExists = left.pExisted || right.pExisted || root == p;
+        bool qExists = left.qExisted || right.qExisted || root == q;
+        if(pExists && qExists) {
+            return new ResultType(true, true, root);
+        }
+        return new ResultType(pExists, qExists, null);
+    }
+}
