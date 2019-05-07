ec571fa4758557e7525f37be895ee0c7eb173253
Hongmin Wang <hongmin.wang@reisystems.com>
2019-05-06 23:48:43 -0400

    Add new solution for q350.

--- Hash/Easy/350-Intersection-of-Two-Arrays-II/solution-Dictionary.cs
+++ Hash/Easy/350-Intersection-of-Two-Arrays-II/solution-Dictionary.cs
@@ -1,26 +1,24 @@
 public class Solution {
     public int[] Intersect(int[] nums1, int[] nums2) {
-        if(nums1.Length == 0 || nums2.Length == 0){//corner case
-            return new int[0]; 
+        // hash table 
+        // tc:O(n); sc:O(n)
+        if(nums1.Length == 0 || nums2.Length == 0){ //corner case
+            return new int[0];
         }
-        Dictionary<int, int> ht = new Dictionary<int, int>();
-        foreach(int num in nums1){ //traverse array nums1, and add each num in Dictionary
-            if(!ht.ContainsKey(num)){
-                ht.Add(num, 1);
-            }
-            else{
-                ht[num]++;
-            }
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        List<int> res = new List<int>();
+        foreach(int num in nums1) {
+            dict[num] = dict.ContainsKey(num) ? ++dict[num] : 1;
         }
-        ICollection<int> collection = new System.Collections.ObjectModel.Collection<int>();
-        foreach(int num in nums2){
-            if(ht.ContainsKey(num)){
-                if(--ht[num] == 0){ //decrease the value by 1, and if value = 0, then remove the key
-                    ht.Remove(num);
+        foreach(int num in nums2) {
+            if(dict.ContainsKey(num)) {
+                res.Add(num);
+                dict[num]--;
+                if(dict[num] == 0) {
+                    dict.Remove(num);
                 }
-                collection.Add(num);
             }
         }
-        return collection.ToArray();
+        return res.ToArray();
     }
 }
---
+++ Hash/Easy/350-Intersection-of-Two-Arrays-II/solution_sort_twopointer.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        // sort + two pointers;
+        // tc:O(nlogn + mlogm); sc:O(1)
+        if(nums1.Length == 0 || nums2.Length == 0){ //corner case
+            return new int[0];
+        }
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        int pos1 = 0, pos2 = 0;
+        List<int> res = new List<int>();
+        while(pos1 < nums1.Length && pos2 < nums2.Length) {
+            if(nums1[pos1] < nums2[pos2]) {
+                pos1++;
+            }
+            else if(nums1[pos1] > nums2[pos2]) {
+                pos2++;
+            }
+            else {           
+                res.Add(nums1[pos1]);
+                pos1++;
+                pos2++;
+            }
+        }
+        return res.ToArray();
+    }
+}
