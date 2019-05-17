8505c304e3150c81fe69fcfaafc9b3cefc3c2f58
Hongmin Wang <hongmin.wang@reisystems.com>
2019-05-16 21:22:39 -0400

    Removed the redundant part of solution for q 445.

--- Linked-List/Medium/445-add-two-numbers-ii/445.add-two-numbers-ii-reverse_3times.cs
+++ Linked-List/Medium/445-add-two-numbers-ii/445.add-two-numbers-ii-reverse_3times.cs
@@ -28,15 +28,7 @@ Output: 7 -> 8 -> 0 -> 7
  *     public ListNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int x) { val = x; }
- * }
- */
- 
+
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
         // reverse list; linked list
