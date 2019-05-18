849ee2998f2dd2677381518c2c1be1ca9c9a364e
Hongmin Wang <hongmin.wang@reisystems.com>
2019-05-18 17:15:57 -0400

    Modified the solution for q54.

--- Array/Medium/54-Spiral-Matrix/solution_v1.0.cs
+++ Array/Medium/54-Spiral-Matrix/solution_v1.0.cs
@@ -1,42 +1,38 @@
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        //matrix
-        //TC:O(n^2); SC:O(1)
-		if (matrix.empty()){//corner case
-			return {};
-		}
-		vector<int> res;
-		int left = 0, right = matrix[0].size() - 1;
-		int top = 0, down = matrix.size() - 1;
-		int dir = 0; //control direction
-        while(left <= right && top <= down){
-            if(dir % 4 == 0){
-                for(int i = left; i <= right; i++){
-                    res.push_back(matrix[top][i]);
-                }
-                top++;
-            }
-            else if(dir % 4 == 1){
-                for(int i = top; i <= down; i++){
-                    res.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            else if(dir % 4 == 2){
-                for(int i = right; i >= left; i--){
-                    res.push_back(matrix[down][i]);
-                }
-                down--;
-            }
-            else if(dir % 4 == 3){
-                for(int i = down; i >= top; i--){
-                    res.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-            dir++;
+public class Solution {
+    public IList<int> SpiralOrder(int[][] matrix) {
+        // array; spiral traversal
+        // tc:O(n^2); sc:O(1)
+        if(matrix == null || matrix.Length == 0) {
+            return new List<int>();
         }
-        return res;
+        IList<int> res = new List<int>();
+        int rows = matrix.Length, cols = matrix[0].Length;
+        int left = 0, right = cols - 1;
+        int up = 0, down = rows - 1;
+        while(left <= right && up <= down) {
+            for(int i = left; i <= right; i++) { // from left to right
+                res.Add(matrix[up][i]);
+            }
+            up++; // the upper most elem covered
+
+            for(int j = up; j <= down; j++) { // from up to down
+                res.Add(matrix[j][right]);
+            }
+            right--;
+            
+            if(left > right || up > down) {
+                break;
+            }
+            for(int k = right; k >= left; k--) { // from right to left
+                res.Add(matrix[down][k]);
+            }
+            down--;
+            
+            for(int l = down; l >= up; l--) { // from down to up
+                res.Add(matrix[l][left]);
+            }
+            left++;
+        }
+        return res; 
     }
-};
+}
