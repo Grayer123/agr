1e9dff85872d9a50fa308c256f3be31c632f8107
Hongmin Wang <hongmin.wang@reisystems.com>
2019-05-30 15:03:21 -0400

    Added comments to solution for q9.

--- Math/Easy/9. Palindrome Number/solution2.cs
+++ Math/Easy/9. Palindrome Number/solution2.cs
@@ -2,16 +2,16 @@ public class Solution {
     public bool IsPalindrome(int x) {
         // math
         // tc:O(log10(n)); sc:O(1)
-        if(x < 0 || x % 10 == 0 && x != 0) {
+        if(x < 0 || x % 10 == 0 && x != 0) {  // corner case: negative num || for num with last digit of 0 
             return false;
         }
         int reversed = 0;
-        while(x > reversed) {
+        while(x > reversed) {  // guarantee to reach at least half of the digits 
             int lastDigit = x % 10;
             reversed = reversed * 10 + lastDigit;
             x /= 10;
         }
-        if(x < reversed) {
+        if(x < reversed) { // eg: 121
             x = x * 10 + reversed % 10;
         }
         return x == reversed;
