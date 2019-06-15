e134841f0755578f72b2faa1be6569b8720aa07f
Hongmin Wang <hongmin.wang@reisystems.com>
2019-06-15 17:35:15 -0400

    Modified dfs solution for q297.

--- Tree/Hard/297-Serialize-and-Deserialize-Binary-Tree/solution_dfs_split.cs
+++ Tree/Hard/297-Serialize-and-Deserialize-Binary-Tree/solution_dfs_split.cs
@@ -10,48 +10,52 @@
 public class Codec {
     // dfs: preorder
     // tc:O(n); sc:O(n)
-    
+
     // Encodes a tree to a single string.
     public string serialize(TreeNode root) {
+        // preorder traversal (iterative)
+        // tc:O(n); sc:O(n)
         if(root == null) {
-            return string.Empty;
+            return String.Empty;
         }
         StringBuilder str = new StringBuilder();
-        PreorderTraverse(root, str);
-        return str.ToString();
-    }
-    
-    private void PreorderTraverse(TreeNode node, StringBuilder str) {
-        if(node == null) {
-            str.Append("# ");
-            return;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.Push(root);
+        while(stack.Count > 0) {
+            TreeNode node = stack.Pop();
+            if(node != null) {
+                str.Append(node.val);
+                stack.Push(node.right); // stack: filo => put right first
+                stack.Push(node.left);               
+            }
+            else {
+                str.Append('#');
+            }
+            str.Append(',');
         }
-        str.Append(node.val + " ");
-        PreorderTraverse(node.left, str);
-        PreorderTraverse(node.right, str);
-        
+        str.Length--; // remove the last ,
+        return str.ToString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(string data) {
-        if(data == null || data == string.Empty) {
+        if(String.IsNullOrEmpty(data)) {
             return null;
         }
-        string[] arr = data.Split(' ');
-        int cur = 0;       
-        return FetchNode(arr, ref cur);
+        string[] strArr = data.Split(',');
+        int idx = 0;
+        return ConstructTree(strArr, ref idx);
     }
     
-    private TreeNode FetchNode(string[] arr, ref int cur) {
-        if(arr[cur] == "#") {
-            cur++;
+    private TreeNode ConstructTree(string[] strArr, ref int idx) {
+        if(idx >= strArr.Length || strArr[idx] == "#") { // null
+            idx++; // need to point to next index
             return null;
         }
-        TreeNode newNode = new TreeNode(Int32.Parse(arr[cur]));
-        cur++;
-        newNode.left = FetchNode(arr, ref cur);
-        newNode.right = FetchNode(arr, ref cur);
-        return newNode;
+        TreeNode node = new TreeNode(Int32.Parse(strArr[idx++]));
+        node.left = ConstructTree(strArr, ref idx);
+        node.right = ConstructTree(strArr, ref idx);
+        return node;
     }
 }
 
